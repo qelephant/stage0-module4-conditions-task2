1dc51130104ae73b5299a7c4257242d8f493b62d
qyerke <70740293+qelephant@users.noreply.github.com>
2023-02-21 11:47:28 +0600

    Update CoinFlip.java

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,11 +2,11 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-        if(from1UpTo1000 < 500) { 
-            System.out.print("Eagle");
+        if(from1UpTo1000 >= 500) { 
+            System.out.print("Tail");
         }
         else { 
-            System.out.print("Tail");
+            System.out.print("Eagle");
         }
     }
 }
